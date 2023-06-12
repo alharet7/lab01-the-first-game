5e54ca1234eb3df371b77c185ad6279b7c4390f2
AlHareth alhyari <hdhareth@gmail.com>
2023-06-12 17:54:19 +0300

    Add project files.

---
+++ lab01-the-first-game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab01_the_first_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
